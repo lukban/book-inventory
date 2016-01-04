8391c381b3d898f4d67df37efdbe8d7ad9585fb4
Mateusz Kwasniewski <kwasniewski.mateusz@gmail.com>
2016-01-04 12:55:07 +0100

    first E2E test

--- server.js
+++ server.js
@@ -1,42 +1,4 @@
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
-
-function logRequest(req, res, next) {
-    console.log('incoming request logged at ' + new Date());
-    next();
-}
-
-app.use(logRequest);
-app.use(bodyParser.json());
-
-app.get('/', function (req, res) {
-    res.send('Hello World!');
-});
-
-app.post('/stock', function(req, res) {
-    res.json({isbn: req.body.isbn, count: req.body.count});
-});
-
-app.use(clientError);
-app.use(serverError);
-
-function clientError(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-}
-
-function serverError(err, req, res, next) {
-    res.status(err.status || 500);
-    console.error(err.stack);
-    res.json({
-        message: err.message,
-        error: (process.env.NODE_ENV === 'production') ? {} : err.toString()
-    });
-}
-
-
+var app = require('./app');
 
 var server = app.listen(3000, function () {
     var host = server.address().address;
