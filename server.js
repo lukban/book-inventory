d47cec181bde406f1546733f50951aa6ce91e5db
Mateusz Kwasniewski <kwasniewski.mateusz@gmail.com>
2016-01-04 11:29:38 +0100

    error handling

--- server.js
+++ server.js
@@ -8,9 +8,29 @@ function logRequest(req, res, next) {
 
 app.use(logRequest);
 
-app.get('/', function (req, res) {
+app.get('/', logRequest, function (req, res) {
     res.send('Hello World!');
 });
+
+app.use(clientError);
+app.use(serverError);
+
+function clientError(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+}
+
+function serverError(err, req, res, next) {
+    res.status(err.status || 500);
+    console.error(err.stack);
+    res.json({
+        message: err.message,
+        error: (process.env.NODE_ENV === 'production') ? {} : err.toString()
+    });
+}
+
+
 
 var server = app.listen(3000, function () {
     var host = server.address().address;
