6769c90dce274aef951fda153fb38d4fa1ff482b
Mateusz Kwasniewski <kwasniewski.mateusz@gmail.com>
2016-01-04 15:21:44 +0100

    check book by isbn

--- app.js
+++ app.js
@@ -31,6 +31,17 @@ app.get('/stock', function (req, res) {
     });
 });
 
+app.get('/stock/:isbn', function (req, res) {
+    stockRepository.getCount(req.params.isbn).then(function (result) {
+        if (result !== null) {
+            res.status(200).json({count: result});
+        } else {
+            res.status(404).json({error: 'No book with ISBN: ' + req.params.isbn});
+        }
+        res.json({});
+    });
+});
+
 app.post('/stock', function (req, res) {
     stockRepository.stockUp(req.body.isbn, req.body.count)
         .then(function (result) {
