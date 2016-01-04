f9142123333d706760bb5429c751197a724cd571
Mateusz Kwasniewski <kwasniewski.mateusz@gmail.com>
2016-01-04 16:31:29 +0100

    move routes to a separate file

--- app.js
+++ app.js
@@ -7,21 +7,6 @@ function logRequest(req, res, next) {
     next();
 }
 
-function clientError(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-}
-
-function serverError(err, req, res, next) {
-    res.status(err.status || 500);
-    console.error(err.stack);
-    res.json({
-        message: err.message,
-        error: (process.env.NODE_ENV === 'production') ? {} : err.toString()
-    });
-}
-
 module.exports = function(stockRepository) {
     app.use(logRequest);
     app.use(bodyParser.json());
@@ -30,38 +15,13 @@ module.exports = function(stockRepository) {
         res.send('Hello World!');
     });
 
-    app.get('/stock', function (req, res) {
-        stockRepository.findAll().then(function (books) {
-            res.json(books);
-        }).catch(function (err) {
-            console.error(err.stack);
-            res.status(500).json({error: "Can't read the stock right now. Try again later."});
-        });
-    });
+    var routes = require('./routes')(stockRepository);
+    app.get('/stock', routes.findAll);
+    app.get('/stock/:isbn', routes.getCount);
+    app.post('/stock', routes.stockUp);
 
-    app.get('/stock/:isbn', function (req, res) {
-        stockRepository.getCount(req.params.isbn).then(function (result) {
-            if (result !== null) {
-                res.status(200).json({count: result});
-            } else {
-                res.status(404).json({error: 'No book with ISBN: ' + req.params.isbn});
-            }
-            res.json({});
-        });
-    });
-
-    app.post('/stock', function (req, res) {
-        stockRepository.stockUp(req.body.isbn, req.body.count)
-            .then(function (result) {
-                res.json({isbn: req.body.isbn, count: req.body.count});
-            }).catch(function (err) {
-                console.error(err.stack);
-                res.status(500).json({error: "Can't stock up right now. Try again later."});
-            });
-    });
-
-    app.use(clientError);
-    app.use(serverError);
+    app.use(routes.stockUp);
+    app.use(routes.serverError);
 
     return app;
 };
